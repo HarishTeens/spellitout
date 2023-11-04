65eee0efea361741a6909fa05a0e683228db5c6f
Sanjay Ganapathi <gana.sanjay12@gmail.com>
2023-11-04 14:47:42 +0530

    deepgram client

--- client_fe/meetingsdk-react/src/capture.js
+++ client_fe/meetingsdk-react/src/capture.js
@@ -1,113 +1,117 @@
 //================= CONFIG =================
 // Global Variables
-const websocket_uri = 'ws://127.0.0.1:5001';
+const websocket_uri = "ws://127.0.0.1:5001";
 const bufferSize = 1024;
 
 let AudioContext, context, processor, input, globalStream, websocket;
 
 // Initialize WebSocket
 initWebSocket();
-setTimeout(()=> {startRecording()}, 2000);  
+setTimeout(() => {
+  startRecording();
+}, 2000);
 
 function initWebSocket() {
-    // Create WebSocket
-    websocket = new WebSocket(websocket_uri);
-    //console.log("Websocket created...");
-  
-    // WebSocket Definitions: executed when triggered webSocketStatus
-    websocket.onopen = function() {
-      console.log("connected to server");
-      //websocket.send("CONNECTED TO YOU");
-    }
-    
-    websocket.onclose = function(e) {
-      console.log("connection closed (" + e.code + ")");
-    }
-    
-    websocket.onmessage = function(e) {
-      //console.log("message received: " + e.data);
-      console.log(e.data);
-  
-    }
+  // Create WebSocket
+  websocket = new WebSocket(websocket_uri);
+  //console.log("Websocket created...");
+
+  // WebSocket Definitions: executed when triggered webSocketStatus
+  websocket.onopen = function () {
+    console.log("connected to server");
+    //websocket.send("CONNECTED TO YOU");
+  };
+
+  websocket.onclose = function (e) {
+    console.log("connection closed (" + e.code + ")");
+  };
+
+  websocket.onmessage = function (e) {
+    //console.log("message received: " + e.data);
+    console.log(e.data);
+  };
 } // closes function initWebSocket()
 
 //================= RECORDING =================
 async function startRecording() {
-    try {
-        AudioContext = window.AudioContext || window.webkitAudioContext;
-        context = new AudioContext({
-            latencyHint: 'interactive',
-        });
-        processor = context.createScriptProcessor(bufferSize, 1, 1);
-        processor.connect(context.destination);
-        await context.resume();
+  try {
+    AudioContext = window.AudioContext || window.webkitAudioContext;
+    context = new AudioContext({
+      latencyHint: "interactive",
+    });
+    processor = context.createScriptProcessor(bufferSize, 1, 1);
+    processor.connect(context.destination);
+    await context.resume();
 
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
-        globalStream = stream;
-        input = context.createMediaStreamSource(stream);
-        input.connect(processor);
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false,
+    });
+    globalStream = stream;
+    input = context.createMediaStreamSource(stream);
+    input.connect(processor);
 
-        processor.onaudioprocess = (e) => {
-            const left = e.inputBuffer.getChannelData(0);
-            const left16 = downsampleBuffer(left, 44100,22021)
-            console.log('sending ', left16)
-            websocket.send(left16);
-        };
-    } catch (error) {
-        console.error('Error starting recording:', error);
-    }
+    processor.onaudioprocess = (e) => {
+      const left = e.inputBuffer.getChannelData(0);
+      const left16 = downsampleBuffer(left, 44100, 16000);
+      console.log("sending ", left16);
+      websocket.send(left16);
+    };
+  } catch (error) {
+    console.error("Error starting recording:", error);
+  }
 }
 
 function stopRecording() {
+  if (globalStream) {
+    const track = globalStream.getTracks()[0];
+    track.stop();
+  }
 
-    if (globalStream) {
-        const track = globalStream.getTracks()[0];
-        track.stop();
-    }
+  if (input) {
+    input.disconnect(processor);
+  }
 
-    if (input) {
-        input.disconnect(processor);
-    }
+  if (processor) {
+    processor.disconnect(context.destination);
+  }
 
-    if (processor) {
-        processor.disconnect(context.destination);
-    }
-
-    if (context) {
-        context.close().then(() => {
-            input = null;
-            processor = null;
-            context = null;
-            AudioContext = null;
-        });
-    }
+  if (context) {
+    context.close().then(() => {
+      input = null;
+      processor = null;
+      context = null;
+      AudioContext = null;
+    });
+  }
 }
 
+function downsampleBuffer(buffer, sampleRate, outSampleRate) {
+  if (outSampleRate === sampleRate) {
+    return buffer;
+  }
+  if (outSampleRate > sampleRate) {
+    throw new Error(
+      "downsampling rate show be smaller than original sample rate"
+    );
+  }
+  var sampleRateRatio = sampleRate / outSampleRate;
+  var newLength = Math.round(buffer.length / sampleRateRatio);
+  var result = new Int16Array(newLength);
+  var offsetResult = 0;
+  var offsetBuffer = 0;
+  while (offsetResult < result.length) {
+    var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
+    var accum = 0,
+      count = 0;
+    for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
+      accum += buffer[i];
+      count++;
+    }
 
-function downsampleBuffer (buffer, sampleRate, outSampleRate) {
-    if (outSampleRate === sampleRate) {
-      return buffer;
-    }
-    if (outSampleRate > sampleRate) {
-      throw new Error('downsampling rate show be smaller than original sample rate');
-    }
-    var sampleRateRatio = sampleRate / outSampleRate;
-    var newLength = Math.round(buffer.length / sampleRateRatio);
-    var result = new Int16Array(newLength);
-    var offsetResult = 0;
-    var offsetBuffer = 0;
-    while (offsetResult < result.length) {
-      var nextOffsetBuffer = Math.round((offsetResult + 1) * sampleRateRatio);
-      var accum = 0,
-        count = 0;
-      for (var i = offsetBuffer; i < nextOffsetBuffer && i < buffer.length; i++) {
-        accum += buffer[i];
-        count++;
-      }
-  
-      result[offsetResult] = Math.min(1, accum / count) * 0x7fff;
-      offsetResult++;
-      offsetBuffer = nextOffsetBuffer;
-    }
-    return result.buffer;
+    result[offsetResult] = Math.min(1, accum / count) * 0x7fff;
+    offsetResult++;
+    offsetBuffer = nextOffsetBuffer;
+  }
+  return result.buffer;
 } // closes function downsampleBuffer()
--- deepgram_test_python/web/client.js
+++ deepgram_test_python/web/client.js
@@ -3,31 +3,25 @@ const stopButton = document.getElementById("stopButton");
 
 let mediaRecorder;
 let socket;
-let audioContext;
-let processor;
-let source;
+
+const API_KEY = "8cba3c162802c613422ced7d43972782fce0b1b0";
 
 startButton.addEventListener("click", startStreaming);
 stopButton.addEventListener("click", stopStreaming);
 
-websocket_uri = "ws://localhost:5000";
-query_params = "&encoding=linear16&sample_rate=16000";
-final_uri = websocket_uri + "?";
+// websocket_uri = "ws://localhost:5000";
+websocket_uri = "wss://api.deepgram.com/v1/listen?model=general&tier=enhanced";
+// final_uri = websocket_uri + "?";
 
 function startStreaming() {
   navigator.mediaDevices
     .getUserMedia({ audio: true })
     .then((stream) => {
-      audioContext = new AudioContext();
-      source = audioContext.createMediaStreamSource(stream);
-      processor = audioContext.createScriptProcessor(1024, 1, 1);
+      console.log({ stream });
       mediaRecorder = new MediaRecorder(stream);
-      mediaRecorder.ondataavailable = onMediaDataAvailable;
-      mediaRecorder.start();
-
-      socket = new WebSocket(websocket_uri);
-
+      socket = new WebSocket(websocket_uri, ["token", API_KEY]);
       socket.onopen = onSocketOpen;
+
       socket.onmessage = onSocketMessage;
 
       startButton.disabled = true;
@@ -39,7 +33,7 @@ function startStreaming() {
 }
 
 function onMediaDataAvailable(event) {
-  if (socket.readyState === WebSocket.OPEN) {
+  if (event.data.size > 0 && socket.readyState === 1) {
     socket.send(event.data);
   }
 }
@@ -57,12 +51,13 @@ function stopStreaming() {
 
 function onSocketOpen() {
   console.log("WebSocket connection is open");
+  mediaRecorder.addEventListener("dataavailable", onMediaDataAvailable);
+  mediaRecorder.start(250);
 }
 
 function onSocketMessage(event) {
-  // Handle the real-time transcription response from Deepgram
   const response = JSON.parse(event.data);
-  if (response.type === "Transcript") {
-    console.log("Transcription: " + response.data.text);
-  }
+  const transcript = response.channel.alternatives[0].transcript;
+
+  console.log(transcript);
 }
