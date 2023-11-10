679b0401a4a2cbb2fe0833257060e78066c131fc
Sanjay Ganapathi <gana.sanjay12@gmail.com>
2023-11-11 01:17:44 +0530

    harish sexy

--- client/spellitout/app/start/page.tsx
+++ client/spellitout/app/start/page.tsx
@@ -1,70 +1,39 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { toast } from "@/components/ui/use-toast";
+import axios from "axios";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
+export const StartPage = () => {
+  const base_url = process.env.NEXT_PUBLIC_SERVER_BASE_URL;
+  const router = useRouter();
 
-export function StartPage() {
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-      username: "",
-    },
-  });
+  const [password, setPassword] = useState("");
 
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    });
-  }
+  const handleSubmit = () => {
+    axios
+      .post(`${base_url}/start`, {
+        password: password,
+      })
+      .then((response) => {
+        console.log("REDIRECTING TO VIEW");
+        router.push("/view");
+      });
+  };
+
+  const handleChange = (event: any) => {
+    setPassword(event.target.value);
+  };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div className="flex w-full max-w-sm items-center space-x-2 bg-slate-800">
+      <Input type="text" placeholder="Password" onChange={handleChange} />
+      <Button type="submit" onClick={handleSubmit}>
+        Subscribe
+      </Button>
+    </div>
   );
-}
-
+};
 export default StartPage;
