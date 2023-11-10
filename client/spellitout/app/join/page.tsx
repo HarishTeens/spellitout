e4b622033506f1feed7055e960bf2a4e659df3c7
Sanjay Ganapathi <gana.sanjay12@gmail.com>
2023-11-11 01:55:53 +0530

    sexy harish

--- client/spellitout/app/join/page.tsx
+++ client/spellitout/app/join/page.tsx
@@ -1,7 +1,67 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
 
 const JoinPage = () => {
-  return <div>JoinPage</div>;
+  const base_url = process.env.NEXT_PUBLIC_SERVER_BASE_URL;
+
+  const [inputLang, setInputLang] = useState("");
+  const [outputLang, setOutputLang] = useState("");
+
+  const handleSubmit = () => {
+    localStorage.setItem("inputLang", inputLang);
+    localStorage.setItem("outputLang", outputLang);
+  };
+
+  return (
+    <main className=" bg-white">
+      <div className="text-slate-800">Sexy Harish</div>
+      <div className="flex items-center justify-evenly gap-2 ">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="">
+            Select Input Language
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Language</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => setInputLang("en")}>
+              English
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setInputLang("es")}>
+              Spanish
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger className="">
+            Select Output Language
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Language</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => setOutputLang("en")}>
+              English
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setOutputLang("es")}>
+              Spanish
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <Button onClick={handleSubmit}>Submit</Button>
+      </div>
+    </main>
+  );
 };
 
 export default JoinPage;
