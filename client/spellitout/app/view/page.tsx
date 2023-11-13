f9df47557e774a9ef04c614bfef0a96f1667ec61
Sanjay Ganapathi <gana.sanjay12@gmail.com>
2023-11-14 00:16:55 +0530

    trash transcript render

--- client/spellitout/app/view/page.tsx
+++ client/spellitout/app/view/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import connectSocket from "@/lib/socket";
 
 export const ViewPage = () => {
   const sock = useRef<any>(null);
+  const [inpLanguage, setInpLanguage] = useState<string>("");
+  const [outLanguage, setOutLanguage] = useState<string>("");
 
   useEffect(() => {
     sock.current = connectSocket();
@@ -12,8 +14,29 @@ export const ViewPage = () => {
     sock.current.on("transcript", (transcript: any) => {
       // captions.innerHTML = transcript ? `<span>${transcript}</span>` : "";
       console.log(transcript);
+      const contents = Object.values(transcript).filter(Boolean);
+
+      console.log(contents);
+
+      if (!contents.length) return;
+      setInpLanguage((lang) => {
+        if (lang.includes(contents[0] as string)) return lang;
+        return lang + " " + contents[0];
+      });
+      setOutLanguage((lang) => {
+        if (lang.includes(contents[1] as string)) return lang;
+        return lang + " " + contents[1];
+      });
     });
   }, []);
-  return <div>ViewPage</div>;
+  return (
+    <>
+      <div>ViewPage</div>
+      <div className="flex items-center gap-4 flex-col">
+        <div className="text-lg max-w-xl">{inpLanguage}</div>
+        <div className="text-lg max-w-xl">{outLanguage}</div>
+      </div>
+    </>
+  );
 };
 export default ViewPage;
