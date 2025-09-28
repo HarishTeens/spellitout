47a292fd204129196ae97e602cd5a91d356ca63a
HarishTeens <arishh2@gmail.com>
2025-09-28 16:00:33 +0530

    add setup script, update deepgram type

--- server/lib/config/types.ts
+++ server/lib/config/types.ts
@@ -1,11 +1,11 @@
-import { LiveTranscription } from "@deepgram/sdk/dist/transcription/liveTranscription";
+import { ListenLiveClient } from "@deepgram/sdk";
 
-export type SupportedLanguages = 'en' | 'es';
+export type SupportedLanguages = "en" | "es";
 export interface InpOut {
-    in: SupportedLanguages;
-    out: SupportedLanguages;
+  in: SupportedLanguages;
+  out: SupportedLanguages;
 }
-export type LangMap = Map<string, InpOut>
+export type LangMap = Map<string, InpOut>;
 export type DeepgramSDKMap = {
-    [key: string]: LiveTranscription;
+  [key: string]: ListenLiveClient;
 };
---
+++ setup.sh
@@ -0,0 +1,3 @@
+sudo apt update
+git clone git@github.com:HarishTeens/spellitout.git
+sudo apt install -y nodejs npm
