d26599a2512ad0939737a4fc06add518cb7da680
HarishTeens <arishh2@gmail.com>
2025-09-28 14:51:06 +0530

    handle google translate errors

--- server/lib/service/gTranslate.ts
+++ server/lib/service/gTranslate.ts
@@ -1,37 +1,41 @@
-
 // Instantiates a client
-import { TranslationServiceClient } from '@google-cloud/translate';
-import { SUPPORTED_LANGUAGES } from '../config/constants';
+import { TranslationServiceClient } from "@google-cloud/translate";
+import { SUPPORTED_LANGUAGES } from "../config/constants";
 const translationClient = new TranslationServiceClient({
-    keyFilename: process.env.GOOGLE_KEY_FILE
+  keyFilename: process.env.GOOGLE_KEY_FILE,
 });
-const projectId = 'spellitout';
-const location = 'global';
+const projectId = "spellitout";
+const location = "global";
 
 export async function translateAll(text, src, response) {
-    const targets = SUPPORTED_LANGUAGES.filter(lang => lang.id !== src);
-    await Promise.all(targets.map(async (target) => {
+  const targets = SUPPORTED_LANGUAGES.filter((lang) => lang.id !== src);
+  try {
+    await Promise.all(
+      targets.map(async (target) => {
         response[target.id] = await translateText(text, src, target.id);
-    }));
+      })
+    );
+  } catch (error) {
+    console.error("Translation error:", error);
+  }
 }
 
 export async function translateText(text, src, target) {
-    if (!text || !text.trim()) return;
-    // Construct request
-    const request = {
-        parent: `projects/${projectId}/locations/${location}`,
-        contents: [text],
-        mimeType: 'text/plain', // mime types: text/plain, text/html
-        sourceLanguageCode: src,
-        targetLanguageCode: target,
-    };
+  if (!text || !text.trim()) return;
+  // Construct request
+  const request = {
+    parent: `projects/${projectId}/locations/${location}`,
+    contents: [text],
+    mimeType: "text/plain", // mime types: text/plain, text/html
+    sourceLanguageCode: src,
+    targetLanguageCode: target,
+  };
 
-    // Run request
-    const [response] = await translationClient.translateText(request);
+  // Run request
+  const [response] = await translationClient.translateText(request);
 
-
-    for (const translation of response.translations) {
-        console.log(`Translation: ${translation.translatedText}`);
-        return translation.translatedText;
-    }
+  for (const translation of response.translations) {
+    console.log(`Translation: ${translation.translatedText}`);
+    return translation.translatedText;
+  }
 }
